541464758dd3e6d7f717898effe9ece2adfdd8a0
Gabor Lakatos <glakatos@asapcloud.com>
2024-07-31 12:20:34 +0200

    Update Azure DevOps link and assembly version

--- obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c69cda84b8da2e69274aa652d46b1cfca51bde")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10da752a327244887ff41c285157057f2b34cf6")]
+>>>>>>> 947ab6a (update welcome page)
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
