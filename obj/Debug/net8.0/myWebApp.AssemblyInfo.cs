8efdcf533b300d70d4c3042820ab1ec559713d75
Gabor Lakatos <glakatos@asapcloud.com>
2024-07-31 12:21:06 +0200

    Update welcome page link and assembly info version

--- obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myWebApp.AssemblyInfo.cs
@@ -14,10 +14,14 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< HEAD
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c69cda84b8da2e69274aa652d46b1cfca51bde")]
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10da752a327244887ff41c285157057f2b34cf6")]
 >>>>>>> 947ab6a (update welcome page)
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947ab6a9d3a49d5bb9ac5cd4e85d7ad9eb86c487")]
+>>>>>>> 157657f (modified index.cshtml)
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
